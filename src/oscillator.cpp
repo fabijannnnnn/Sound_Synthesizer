fc1e5ba8bbd094265457256c0dd82a52f2ff488e
fabijannn <fabianuhrin1@gmail.com>
2025-09-18 03:33:58 +0200

    working oscillator

--- src/oscillator.cpp
+++ src/oscillator.cpp
@@ -1,3 +1,65 @@
-//
-// Created by fabijannnnnn . on 17/09/2025.
-//
+#include "oscillator.h"
+#include <stdexcept>
+
+Oscillator::Oscillator(float sampleRate)
+        : sampleRate_(sampleRate), phase_(0.0f), frequency_(440.0f),
+          amplitude_(0.5f), waveform_(Waveform::SINE) {
+    if (sampleRate <= 0) {
+        throw std::invalid_argument("Sample rate must be greater than 0");
+    }
+}
+
+float Oscillator::process() {
+    float sample = 0.0f;
+
+    switch (waveform_) {
+        case Waveform::SINE:
+            sample = generateSine();
+            break;
+        case Waveform::SQUARE:
+            sample = generateSquare();
+            break;
+        case Waveform::SAW:
+            sample = generateSaw();
+            break;
+        case Waveform::TRIANGLE:
+            sample = generateTriangle();
+            break;
+    }
+
+    // Update phase for next sample
+    phase_ += frequency_ / sampleRate_;
+    if (phase_ >= 1.0f) {
+        phase_ -= 1.0f;
+    }
+
+    return sample * amplitude_;
+}
+
+float Oscillator::generateSine() {
+    return std::sin(2.0f * M_PI * phase_);
+}
+
+float Oscillator::generateSquare() {
+    return (phase_ < 0.5f) ? 1.0f : -1.0f;
+}
+
+float Oscillator::generateSaw() {
+    return 2.0f * phase_ - 1.0f;
+}
+
+float Oscillator::generateTriangle() {
+    float value = -1.0f + (2.0f * phase_);
+    return 2.0f * (std::fabs(value) - 0.5f);
+}
+
+// Global operator overloads
+Oscillator operator+(Oscillator osc, float frequency) {
+    osc.setFrequency(osc.getFrequency() + frequency);
+    return osc;
+}
+
+Oscillator operator-(Oscillator osc, float frequency) {
+    osc.setFrequency(osc.getFrequency() - frequency);
+    return osc;
+}
--- src/oscillator.h
+++ src/oscillator.h
@@ -1,8 +1,62 @@
-//
-// Created by fabijannnnnn . on 17/09/2025.
-//
+#ifndef OSCILLATOR_H
+#define OSCILLATOR_H
 
-#ifndef SYNTHESIZER_OSCILLATOR_H
-#define SYNTHESIZER_OSCILLATOR_H
+#include <cmath>
 
-#endif //SYNTHESIZER_OSCILLATOR_H
+class Oscillator {
+public:
+    enum class Waveform {
+        SINE,
+        SQUARE,
+        SAW,
+        TRIANGLE
+    };
+
+    Oscillator(float sampleRate = 44100.0f);
+
+    // Set waveform type
+    void setWaveform(Waveform waveform) { waveform_ = waveform; }
+
+    // Set frequency in Hz
+    void setFrequency(float frequency) { frequency_ = frequency; }
+
+    // Set amplitude (0.0 to 1.0)
+    void setAmplitude(float amplitude) { amplitude_ = amplitude; }
+
+    // Generate the next sample
+    float process();
+
+    // Operator overloading for fun!
+    Oscillator& operator+=(float frequency) {
+        frequency_ += frequency;
+        return *this;
+    }
+
+    Oscillator& operator-=(float frequency) {
+        frequency_ -= frequency;
+        return *this;
+    }
+
+    float getFrequency() const { return frequency_; }
+    float getAmplitude() const { return amplitude_; }
+    Waveform getWaveform() const { return waveform_; }
+
+private:
+    float sampleRate_;
+    float phase_;
+    float frequency_;
+    float amplitude_;
+    Waveform waveform_;
+
+    // Waveform generation functions
+    float generateSine();
+    float generateSquare();
+    float generateSaw();
+    float generateTriangle();
+};
+
+// Global operator overloads for Oscillator
+Oscillator operator+(Oscillator osc, float frequency);
+Oscillator operator-(Oscillator osc, float frequency);
+
+#endif // OSCILLATOR_H
